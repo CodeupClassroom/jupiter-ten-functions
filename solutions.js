06c444e33e71de3b3a9ad33698d6a0c6912cee06
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2020-09-30 11:47:06 -0500

    practice assessment

--- solutions.js
+++ solutions.js
@@ -20,3 +20,41 @@
  * and
  * concat
  */
+function isTrue(input){
+    return input === true;
+}
+
+function isFalse(input){
+    return input === false;
+}
+
+function not(input){
+    return input !== true;
+}
+function addOne(input){
+    return Number(input + 1);
+}
+
+function or(input){
+    return input;
+}
+
+
+function isEqual(input) {
+    return input == !true
+}
+
+function isIdentical(input){
+    return input === true;
+}
+
+function and(input){
+    return input == true;
+}
+
+function concat(input){
+    return input;
+}
+function isEven(input){
+    return input == even ;
+}

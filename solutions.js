4e4ff15a21d06e88a6a2ea4aa2908e1fe069097f
Kaitlin Muse <kaitlinkelleymuse@gmail.com>
2020-09-30 10:20:25 -0500

    finished isTrue and isFalse

--- solutions.js
+++ solutions.js
@@ -20,3 +20,16 @@
  * and
  * concat
  */
+
+// 1. Define a function named `isTrue` that takes in any input and returns true if
+//     the input provided is exactly equal to `true` in value and data type.
+
+function isTrue(input){
+    return input === true;
+}
+
+// 2. Define a function named `isFalse` that takes in a value and returns a true if and only if the provided input is equal to false in both type and value.
+
+function isFalse(input){
+    return input === false;
+}

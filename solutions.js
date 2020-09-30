d3723f5ea7c4c2bbdc7e51fb0632b8355fb187c2
Ryan Phillips <ryan.phillips.j92@gmail.com>
2020-09-30 11:15:27 -0500

    updated exercise

--- solutions.js
+++ solutions.js
@@ -28,23 +28,63 @@ function isTrue(input){
 }
 // 2. Define a function named `isFalse` that takes in a value and returns a true if and only if the provided input is equal to false in both type and value.
 function isFalse(input){
-    if(input)
+    if(input === false){
+        return true;
+    } else {
+        return false;
+    }
 }
 
 // 3. Define a function named `not` that takes in any input and returns the boolean opposite of the provided input.
-
+function not(input){
+   if(input === true){
+       return false;
+   } else {
+       return true;
+   }
+}
 
 // 4. Define a function named `addOne` that takes in a single input. If the input is a number or a numeric string, return the value plus one.
+function addOne(input){
+    if(input === Number)
+        return (1 + input);
+}
 
+console.log(addOne(-4));
+console.log(addOne(Infinity));
+console.log(addOne(45));
+console.log(addOne("0"));
 
 // 5. Define a function named `isEven` that takes in a single input. If the input is an even number or a string containing an even number, return `true`. Any other input should return false for the output.
 
+function isEven(input) {
+    var userInput = parseInt(input);
+    if (userInput % 2 !== 1) {
+        return true;
+    }else {
+        return false;
+    }
+}
 
+console.log(isEven("banana"));
+console.log(isEven(false));
 // 6. Define a function named `isIdentical` that takes in two input arguments. If each input is equal both in data type and in value, then return `true`. If the values are not the same data type or not the same value, return `false`.
-
+function isIdentical(x, y){
+    if(x === y) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // 7. Define a function named `isEqual` that takes in two input arguments. If each argument is equal only in value, then return true. Otherwise return false.
-
+function isEqual(x, y){
+    if(x == y){
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // 8. Define a function named `or` that takes in two input arguments. The output returned should be the result of an `or` operation on both inputs.
 
